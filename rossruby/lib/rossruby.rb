5d085f8584aec2118c9ccc317dc059bed51debf4
gkeary <gregg@gkeary.com>
2010-03-23 05:25:21 -0400

    after running s4t-utils/bin/make... (LF will be replaced by CRLF in s4t-generated files

---
+++ rossruby/lib/rossruby.rb
@@ -0,0 +1,14 @@
+# Require external libraries (whether in third-party or site_ruby) here:
+require 's4t-utils'
+
+# Require all library files in this package here:
+require 'rossruby/version'
+# require 'rossruby/some-file'
+# require 'rossruby/another-file'
+
+# Any comments written here will become
+# the documentation for Ross.
+# That will be the first page shown when you 
+# create the HTML documentation.
+module Ross
+end
---
+++ rossruby/lib/rossruby/version.rb
@@ -0,0 +1,3 @@
+module Ross
+  Version = '0.1.0'
+end
